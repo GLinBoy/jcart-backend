35c6836eeb278bd91a522b6d379f443a02e830bc
Hojjat Abedie <hojjatabedie@gmail.com>
2018-11-21 14:32:03 +0330

    upgrade to java 11

--- src/main/java/ir/sargoll/shop/model/Order.java
+++ src/main/java/ir/sargoll/shop/model/Order.java
@@ -1,6 +1,5 @@
 package ir.sargoll.shop.model;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import lombok.Getter;
 import lombok.Setter;
