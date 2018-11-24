1c56d40142bcea35cc65841ae161df686685a454
Hojjat Abedie <hojjatabedie@gmail.com>
2018-11-24 20:03:02 +0330

    fix anonymous user request data

--- src/main/java/ir/sargoll/shop/security/DataGuard.java
+++ src/main/java/ir/sargoll/shop/security/DataGuard.java
@@ -8,9 +8,13 @@ import ir.sargoll.shop.model.UserPrincipal;
 @Component
 public class DataGuard {
 
-    public boolean checkUserId(Authentication authentication, Long id) {
-    	UserPrincipal user = ((UserPrincipal)authentication.getPrincipal());
-        return (user.getId().equals(id) || user.getAuthorities().stream()
-        		.anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN")));
-    }
+	public boolean checkUserId(Authentication authentication, Long id) {
+		if (authentication.getPrincipal() != null && authentication.getPrincipal() instanceof UserPrincipal) {
+			UserPrincipal user = ((UserPrincipal) authentication.getPrincipal());
+			return (user.getId().equals(id)
+					|| user.getAuthorities().stream().anyMatch(a -> a.getAuthority().equals("ROLE_ADMIN")));
+		} else {
+			return false;
+		}
+	}
 }
