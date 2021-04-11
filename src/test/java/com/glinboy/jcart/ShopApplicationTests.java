c6b419fb11db26b5e2a1ab7255118634a7adc9a2
GLinBoy <hojjatabedie@gmail.com>
2021-04-11 13:01:02 +0430

    👌 IMPROVE: Fix test

--- src/test/java/com/glinboy/jcart/ShopApplicationTests.java
+++ src/test/java/com/glinboy/jcart/ShopApplicationTests.java
@@ -2,16 +2,13 @@ package com.glinboy.jcart;
 
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
-public class ShopApplicationTests {
+class ShopApplicationTests {
 
 	@Test
-	public void contextLoads() {
+	void contextLoads() {
 	}
 
 }
