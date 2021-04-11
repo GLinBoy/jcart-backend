a1f9ba522ae80127ba383183a87a08e5f66c3912
GLinBoy <hojjatabedie@gmail.com>
2021-04-11 12:57:29 +0430

    👌 IMPROVE: Update swagger v2 -> v3
    resolved #6
    fixed #12

--- src/main/java/com/glinboy/jcart/configuration/SecurityConfig.java
+++ src/main/java/com/glinboy/jcart/configuration/SecurityConfig.java
@@ -25,6 +25,24 @@ import com.glinboy.jcart.security.JwtAuthenticationFilter;
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	private static final String[] AUTH_WHITELIST = {
+			"/auth/**",
+			"/users/checkEmailAvailability",
+			"/h2-console/**",
+            // -- Swagger UI v2
+            "/v2/api-docs",
+            "/swagger-resources",
+            "/swagger-resources/**",
+            "/configuration/ui",
+            "/configuration/security",
+            "/swagger-ui.html",
+            "/webjars/**",
+            // -- Swagger UI v3 (OpenAPI)
+            "/v3/api-docs/**",
+            "/swagger-ui/**"
+            // other public endpoints of your API may be appended to this array
+    };
 
 	@Autowired
 	CustomUserDetailsService customUserDetailsService;
@@ -55,14 +73,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.headers().frameOptions().disable();
-		http.cors().and().csrf().ignoringAntMatchers("/auth/**", "/h2-console/**").and().exceptionHandling()
-				.authenticationEntryPoint(unauthorizedHandler).and().sessionManagement()
-				.sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests()
-				.antMatchers("/users/{user_id}/**").access("@dataGuard.checkUserId(authentication,#user_id)")
-				.antMatchers("/auth/**").permitAll().antMatchers("/users/checkEmailAvailability").permitAll()
-				.antMatchers("/h2-console/**").permitAll().antMatchers("/v2/api-docs", "/configuration/ui",
-						"/swagger-resources/**", "/configuration/security", "/swagger-ui.html", "/webjars/**")
-				.permitAll().anyRequest().authenticated();
+		http.cors().and().csrf()
+			.ignoringAntMatchers("/auth/**", "/h2-console/**")
+			.and()
+			.exceptionHandling()
+			.authenticationEntryPoint(unauthorizedHandler)
+			.and()
+			.sessionManagement()
+			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+			.authorizeRequests()
+			.antMatchers("/users/{user_id}/**")
+			.access("@dataGuard.checkUserId(authentication,#user_id)")
+			.antMatchers(AUTH_WHITELIST)
+			.permitAll()
+			.anyRequest()
+			.authenticated();
 		// Add our custom JWT security filter
 		http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
 		http.addFilterAfter(new CsrfHeaderFilter(), CsrfFilter.class);
