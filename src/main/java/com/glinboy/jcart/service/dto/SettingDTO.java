595e9c1e5533ad5f992153d1765d231e4c66cc22
GLinBoy <hojjatabedie@gmail.com>
2021-07-06 15:58:59 +0430

    📦 NEW: Added Setting DTO
    #37

---
+++ src/main/java/com/glinboy/jcart/service/dto/SettingDTO.java
@@ -0,0 +1,10 @@
+package com.glinboy.jcart.service.dto;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper = true)
+public class SettingDTO extends BaseDTO {
+
+}
