f2c5c0f7efb8f35f6f90408cfc95a42a935df085
GLinBoy <hojjatabedie@gmail.com>
2021-07-07 20:39:15 +0430

    📦 NEW: Added Product DTO
    #37

---
+++ src/main/java/com/glinboy/jcart/service/dto/ProductDTO.java
@@ -0,0 +1,21 @@
+package com.glinboy.jcart.service.dto;
+
+import java.util.List;
+import java.util.Set;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+@Data
+@EqualsAndHashCode(callSuper=true)
+public class ProductDTO extends BaseDTO {
+	private String name;
+	private String description;
+	private List<ProductAttributeDTO> attributes;
+	private Double price;
+	private Double rate;
+	private String code;
+	private List<ProductCategoryDTO> categories;
+	private List<ImageDTO> images;
+	private Set<DiscountDTO> discounts;
+}
