971eb3d35cb31580b12ba6d1f080f15be7725611
GLinBoy <hojjatabedie@gmail.com>
2021-07-10 16:33:13 +0430

    👌 IMPROVE: OrderServiceApi use OrderDTO
    #43

--- src/main/java/com/glinboy/jcart/service/OrderServiceApi.java
+++ src/main/java/com/glinboy/jcart/service/OrderServiceApi.java
@@ -7,12 +7,18 @@ import org.springframework.data.domain.Pageable;
 
 import com.glinboy.jcart.model.Order;
 import com.glinboy.jcart.model.ProductOrderItem;
+import com.glinboy.jcart.service.dto.OrderDTO;
 
-public interface OrderServiceApi extends GenericService<Order> {
-    Page<Order> getUserOrders(Long userId, Pageable pageable);
-    Optional<Order> getOrderByUser(Long userId, Long orderId);
-    Optional<Order> getCart(Long userId);
-    Order addToCart(Long userId, ProductOrderItem product);
-    void deleteFromCart(Long userId, Long orderItemId);
-    ProductOrderItem updateOrderItemNumber(Long userId, Long orderItemId, Integer number);
+public interface OrderServiceApi extends GenericService<OrderDTO> {
+	Page<OrderDTO> getUserOrders(Long userId, Pageable pageable);
+
+	Optional<OrderDTO> getOrderByUser(Long userId, Long orderId);
+
+	Optional<OrderDTO> getCart(Long userId);
+
+	Order addToCart(Long userId, ProductOrderItem product);
+
+	void deleteFromCart(Long userId, Long orderItemId);
+
+	ProductOrderItem updateOrderItemNumber(Long userId, Long orderItemId, Integer number);
 }
