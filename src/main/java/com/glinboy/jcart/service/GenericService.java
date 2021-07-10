d3bc6e57df45f2e0fbfed49833d1e59fe9b212ad
GLinBoy <hojjatabedie@gmail.com>
2021-07-10 16:24:46 +0430

    👌 IMPROVE: GenericService accept object extended BaseDTO
    #43

--- src/main/java/com/glinboy/jcart/service/GenericService.java
+++ src/main/java/com/glinboy/jcart/service/GenericService.java
@@ -3,13 +3,22 @@ package com.glinboy.jcart.service;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-public interface GenericService<T> {
-    T save(T t);
-    T update(T t);
-    T getSingleById(Long id);
-    Page<T> getAll (Pageable pageable);
-    Page<T> findAll();
-    void deleteSingleById(Long id);
-    void deleteSingleByReference(T t);
-    void deleteAll();
+import com.glinboy.jcart.service.dto.BaseDTO;
+
+public interface GenericService<T extends BaseDTO> {
+	T save(T t);
+
+	T update(T t);
+
+	T getSingleById(Long id);
+
+	Page<T> getAll(Pageable pageable);
+
+	Page<T> findAll();
+
+	void deleteSingleById(Long id);
+
+	void deleteSingleByReference(T t);
+
+	void deleteAll();
 }
