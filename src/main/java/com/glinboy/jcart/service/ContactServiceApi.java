246de555fc281187b78ff1df344943764784140f
GLinBoy <hojjatabedie@gmail.com>
2021-07-10 16:29:57 +0430

    👌 IMPROVE: ContactServiceApi use Contact
    #43

--- src/main/java/com/glinboy/jcart/service/ContactServiceApi.java
+++ src/main/java/com/glinboy/jcart/service/ContactServiceApi.java
@@ -1,6 +1,6 @@
 package com.glinboy.jcart.service;
 
-import com.glinboy.jcart.model.Contact;
+import com.glinboy.jcart.service.dto.ContactDTO;
 
-public interface ContactServiceApi extends GenericService<Contact> {
+public interface ContactServiceApi extends GenericService<ContactDTO> {
 }
