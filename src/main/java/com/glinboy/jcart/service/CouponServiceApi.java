898f9393f0f9dc8196fe2e28ec0949c3867dac0a
GLinBoy <hojjatabedie@gmail.com>
2021-07-10 16:29:08 +0430

    👌 IMPROVE: CouponServiceApi Use CouponDTO
    #43

--- src/main/java/com/glinboy/jcart/service/CouponServiceApi.java
+++ src/main/java/com/glinboy/jcart/service/CouponServiceApi.java
@@ -1,9 +1,11 @@
 package com.glinboy.jcart.service;
 
 import com.glinboy.jcart.model.Coupon;
+import com.glinboy.jcart.service.dto.CouponDTO;
 
-public interface CouponServiceApi extends GenericService<Coupon> {
-    Boolean verify(String coupon);
+public interface CouponServiceApi extends GenericService<CouponDTO> {
+	
+	Boolean verify(String coupon);
 
-    Coupon disableCoupon(Long id);
+	Coupon disableCoupon(Long id);
 }
