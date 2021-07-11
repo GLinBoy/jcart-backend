2e3b45df8d0dfec209fc50df1635be507cbb4716
GLinBoy <hojjatabedie@gmail.com>
2021-07-11 19:55:42 +0430

    👌 IMPROVE: UserAddressServiceApi use UserAddressDTO
    #43

--- src/main/java/com/glinboy/jcart/service/UserAddressServiceApi.java
+++ src/main/java/com/glinboy/jcart/service/UserAddressServiceApi.java
@@ -3,11 +3,14 @@ package com.glinboy.jcart.service;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import com.glinboy.jcart.model.UserAddress;
+import com.glinboy.jcart.service.dto.UserAddressDTO;
 
-public interface UserAddressServiceApi extends GenericService<UserAddress> {
-    Page<UserAddress> getUserAddresses(Long userId, Pageable pageable);
-    UserAddress getAddressByUser(Long userId, Long addressId);
-    UserAddress addAddressToUser(Long userId, UserAddress address);
-    void deleteAddress(Long userId, Long addressId);
+public interface UserAddressServiceApi extends GenericService<UserAddressDTO> {
+	Page<UserAddressDTO> getUserAddresses(Long userId, Pageable pageable);
+
+	UserAddressDTO getAddressByUser(Long userId, Long addressId);
+
+	UserAddressDTO addAddressToUser(Long userId, UserAddressDTO address);
+
+	void deleteAddress(Long userId, Long addressId);
 }
