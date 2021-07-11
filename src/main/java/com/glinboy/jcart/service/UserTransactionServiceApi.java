7032ab7f674da3f7570abfecd38e6a2899590ab5
GLinBoy <hojjatabedie@gmail.com>
2021-07-11 19:56:39 +0430

    👌 IMPROVE: UserTransactionServiceApi use UserTransactionDTO
    #43

--- src/main/java/com/glinboy/jcart/service/UserTransactionServiceApi.java
+++ src/main/java/com/glinboy/jcart/service/UserTransactionServiceApi.java
@@ -5,11 +5,12 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import com.glinboy.jcart.model.UserTransaction;
+import com.glinboy.jcart.service.dto.UserTransactionDTO;
 
-public interface UserTransactionServiceApi extends GenericService<UserTransaction> {
+public interface UserTransactionServiceApi extends GenericService<UserTransactionDTO> {
 
-    Page<UserTransaction> findUserTransactions(Long userId, Pageable pageable);
-    Optional<UserTransaction> findByUserAndTransaction(Long userId, Long transactionId);
+	Page<UserTransactionDTO> findUserTransactions(Long userId, Pageable pageable);
+
+	Optional<UserTransactionDTO> findByUserAndTransaction(Long userId, Long transactionId);
 
 }
