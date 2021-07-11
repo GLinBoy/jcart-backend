32f78d058ada4b0274b89819062b857c676c6e01
GLinBoy <hojjatabedie@gmail.com>
2021-07-11 19:56:20 +0430

    👌 IMPROVE: UserServiceApi use UserDTO
    #43

--- src/main/java/com/glinboy/jcart/service/UserServiceApi.java
+++ src/main/java/com/glinboy/jcart/service/UserServiceApi.java
@@ -1,10 +1,13 @@
 package com.glinboy.jcart.service;
 
 import com.glinboy.jcart.model.User;
+import com.glinboy.jcart.service.dto.UserDTO;
 
-public interface UserServiceApi extends GenericService<User> {
+public interface UserServiceApi extends GenericService<UserDTO> {
 
-    User register(User user);
-    void recoveryByMail(String mail);
-    void recoveryByMobile(String phoneNumber);
+	User register(UserDTO user);
+
+	void recoveryByMail(String mail);
+
+	void recoveryByMobile(String phoneNumber);
 }
