1e2b9c10c99d0e285d520bbfd605d84b3f70d81f
GLinBoy <hojjatabedie@gmail.com>
2021-07-11 19:55:01 +0430

    👌 IMPROVE: ProductServiceApi use ProductDTO
    #43

--- src/main/java/com/glinboy/jcart/service/ProductServiceApi.java
+++ src/main/java/com/glinboy/jcart/service/ProductServiceApi.java
@@ -3,8 +3,8 @@ package com.glinboy.jcart.service;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
-import com.glinboy.jcart.model.ProductShopItem;
+import com.glinboy.jcart.service.dto.ProductShopItemDTO;
 
-public interface ProductServiceApi extends GenericService<ProductShopItem> {
-    Page<ProductShopItem> getProductsCategory(Long id, Pageable pageable);
+public interface ProductServiceApi extends GenericService<ProductShopItemDTO> {
+	Page<ProductShopItemDTO> getProductsCategory(Long id, Pageable pageable);
 }
