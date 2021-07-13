bd123badf1ba3d8724699a23dfed773145edc1cf
GLinBoy <hojjatabedie@gmail.com>
2021-07-13 21:24:52 +0430

    👌 IMPROVE: Change return type of disableCoupon to its DTO

--- src/main/java/com/glinboy/jcart/service/CouponServiceApi.java
+++ src/main/java/com/glinboy/jcart/service/CouponServiceApi.java
@@ -1,11 +1,10 @@
 package com.glinboy.jcart.service;
 
-import com.glinboy.jcart.model.Coupon;
 import com.glinboy.jcart.service.dto.CouponDTO;
 
 public interface CouponServiceApi extends GenericService<CouponDTO> {
 	
 	Boolean verify(String coupon);
 
-	Coupon disableCoupon(Long id);
+	CouponDTO disableCoupon(Long id);
 }
