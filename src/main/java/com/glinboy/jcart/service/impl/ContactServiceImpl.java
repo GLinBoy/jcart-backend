0360608791923afd396ddae0295a523add067c13
GLinBoy <hojjatabedie@gmail.com>
2021-07-13 21:19:45 +0430

    👌 IMPROVE: Update extends

--- src/main/java/com/glinboy/jcart/service/impl/ContactServiceImpl.java
+++ src/main/java/com/glinboy/jcart/service/impl/ContactServiceImpl.java
@@ -7,12 +7,14 @@ import org.springframework.stereotype.Service;
 import com.glinboy.jcart.model.Contact;
 import com.glinboy.jcart.repository.ContactRepositoryApi;
 import com.glinboy.jcart.service.ContactServiceApi;
+import com.glinboy.jcart.service.dto.ContactDTO;
+import com.glinboy.jcart.service.mapper.ContactMapper;
 
 @Service
 @Transactional
-public class ContactServiceImpl extends AbstractServiceImpl<Contact, ContactRepositoryApi> implements ContactServiceApi {
+public class ContactServiceImpl extends AbstractServiceImpl<ContactDTO, Contact, ContactMapper, ContactRepositoryApi> implements ContactServiceApi {
 
-	public ContactServiceImpl(ContactRepositoryApi repository) {
-		super(repository);
+	public ContactServiceImpl(ContactRepositoryApi repository, ContactMapper mapper) {
+		super(repository, mapper);
 	}
 }
