5b13a2d9ec64ee738734705531b67164ec3d6372
GLinBoy <hojjatabedie@gmail.com>
2021-07-13 21:21:44 +0430

    👌 IMPROVE: Update extends

--- src/main/java/com/glinboy/jcart/service/impl/ContentServiceImpl.java
+++ src/main/java/com/glinboy/jcart/service/impl/ContentServiceImpl.java
@@ -8,14 +8,16 @@ import com.glinboy.jcart.model.Content;
 import com.glinboy.jcart.model.ResourceNotFoundException;
 import com.glinboy.jcart.repository.ContentRepositoryApi;
 import com.glinboy.jcart.service.ContentServiceApi;
+import com.glinboy.jcart.service.dto.ContentDTO;
+import com.glinboy.jcart.service.mapper.ContentMapper;
 
 @Service
 @Transactional
-public class ContentServiceImpl extends AbstractServiceImpl<Content, ContentRepositoryApi>
+public class ContentServiceImpl extends AbstractServiceImpl<ContentDTO, Content, ContentMapper, ContentRepositoryApi>
 		implements ContentServiceApi {
 
-	public ContentServiceImpl(ContentRepositoryApi repository) {
-		super(repository);
+	public ContentServiceImpl(ContentRepositoryApi repository, ContentMapper mapper) {
+		super(repository, mapper);
 	}
 
 	@Override
