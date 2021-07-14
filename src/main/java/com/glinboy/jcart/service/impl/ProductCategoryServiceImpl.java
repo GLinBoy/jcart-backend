9721d8ad3b44542ead4c2fa49e24ae887721f375
GLinBoy <hojjatabedie@gmail.com>
2021-07-14 21:31:25 +0430

    👌 IMPROVE: Update extends

--- src/main/java/com/glinboy/jcart/service/impl/ProductCategoryServiceImpl.java
+++ src/main/java/com/glinboy/jcart/service/impl/ProductCategoryServiceImpl.java
@@ -9,14 +9,17 @@ import org.springframework.stereotype.Service;
 import com.glinboy.jcart.model.ProductCategory;
 import com.glinboy.jcart.repository.ProductCategoryRepositoryApi;
 import com.glinboy.jcart.service.ProductCategoryServiceApi;
+import com.glinboy.jcart.service.dto.ProductCategoryDTO;
+import com.glinboy.jcart.service.mapper.ProductCategoryMapper;
 
 @Service
 @Transactional
-public class ProductCategoryServiceImpl extends AbstractServiceImpl<ProductCategory, ProductCategoryRepositoryApi>
+public class ProductCategoryServiceImpl extends
+		AbstractServiceImpl<ProductCategoryDTO, ProductCategory, ProductCategoryMapper, ProductCategoryRepositoryApi>
 		implements ProductCategoryServiceApi {
-	
-	public ProductCategoryServiceImpl(ProductCategoryRepositoryApi repository) {
-		super(repository);
+
+	public ProductCategoryServiceImpl(ProductCategoryRepositoryApi repository, ProductCategoryMapper mapper) {
+		super(repository, mapper);
 	}
 
 	@Override
