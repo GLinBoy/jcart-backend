c45f9ef0d2f727673cd1d6c7eeeebcc9f332adf7
GLinBoy <hojjatabedie@gmail.com>
2021-07-14 21:09:39 +0430

    👌 IMPROVE: Update extends

--- src/main/java/com/glinboy/jcart/service/impl/OrderItemServiceImpl.java
+++ src/main/java/com/glinboy/jcart/service/impl/OrderItemServiceImpl.java
@@ -7,13 +7,17 @@ import org.springframework.stereotype.Service;
 import com.glinboy.jcart.model.ProductOrderItem;
 import com.glinboy.jcart.repository.OrderItemRepositoryApi;
 import com.glinboy.jcart.service.OrderItemServiceApi;
+import com.glinboy.jcart.service.dto.ProductOrderItemDTO;
+import com.glinboy.jcart.service.mapper.ProductOrderItemMapper;
 
 @Service
 @Transactional
-public class OrderItemServiceImpl extends AbstractServiceImpl<ProductOrderItem, OrderItemRepositoryApi> implements OrderItemServiceApi {
+public class OrderItemServiceImpl extends
+		AbstractServiceImpl<ProductOrderItemDTO, ProductOrderItem, ProductOrderItemMapper, OrderItemRepositoryApi>
+		implements OrderItemServiceApi {
 
-	public OrderItemServiceImpl(OrderItemRepositoryApi repository) {
-		super(repository);
+	public OrderItemServiceImpl(OrderItemRepositoryApi repository, ProductOrderItemMapper mapper) {
+		super(repository, mapper);
 	}
 
 }
