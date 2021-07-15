8d78851d7a278ea8f33aa203f05eba23547c1fdd
GLinBoy <hojjatabedie@gmail.com>
2021-07-15 20:06:19 +0430

    👌 IMPROVE: Update extends

--- src/main/java/com/glinboy/jcart/service/impl/ProductCommentServiceImpl.java
+++ src/main/java/com/glinboy/jcart/service/impl/ProductCommentServiceImpl.java
@@ -7,13 +7,17 @@ import org.springframework.stereotype.Service;
 import com.glinboy.jcart.model.ProductComment;
 import com.glinboy.jcart.repository.ProductCommentRepositoryApi;
 import com.glinboy.jcart.service.ProductCommentServiceApi;
+import com.glinboy.jcart.service.dto.ProductCommentDTO;
+import com.glinboy.jcart.service.mapper.ProductCommentMapper;
 
 @Service
 @Transactional
-public class ProductCommentServiceImpl extends AbstractServiceImpl<ProductComment, ProductCommentRepositoryApi> implements ProductCommentServiceApi {
+public class ProductCommentServiceImpl
+		extends AbstractServiceImpl<ProductCommentDTO, ProductComment, ProductCommentMapper, ProductCommentRepositoryApi>
+		implements ProductCommentServiceApi {
 
-	public ProductCommentServiceImpl(ProductCommentRepositoryApi repository) {
-		super(repository);
+	public ProductCommentServiceImpl(ProductCommentRepositoryApi repository, ProductCommentMapper mapper) {
+		super(repository, mapper);
 	}
 
 }
