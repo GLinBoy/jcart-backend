694a8a16330c3a30229c356ec1254af906d66937
GLinBoy <hojjatabedie@gmail.com>
2021-07-15 20:12:58 +0430

    👌 IMPROVE: Update findByUserAndId to return optional

--- src/main/java/com/glinboy/jcart/repository/UserAddressRepositoryApi.java
+++ src/main/java/com/glinboy/jcart/repository/UserAddressRepositoryApi.java
@@ -1,5 +1,7 @@
 package com.glinboy.jcart.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -7,7 +9,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.glinboy.jcart.model.UserAddress;
 
 public interface UserAddressRepositoryApi extends JpaRepository<UserAddress, Long> {
-    Page<UserAddress> findByUser(Long userId, Pageable pageable);
-    UserAddress findByUserAndId(Long userId, Long addressId);
-    void deleteByUserAndId(Long userId, Long addressId);
+	Page<UserAddress> findByUser(Long userId, Pageable pageable);
+
+	Optional<UserAddress> findByUserAndId(Long userId, Long addressId);
+
+	void deleteByUserAndId(Long userId, Long addressId);
 }
