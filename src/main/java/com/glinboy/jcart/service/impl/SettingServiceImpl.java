c53745cc3bb303e99446086dabdabf6e4eaf6357
GLinBoy <hojjatabedie@gmail.com>
2021-07-15 20:09:05 +0430

    👌 IMPROVE: Update extends

--- src/main/java/com/glinboy/jcart/service/impl/SettingServiceImpl.java
+++ src/main/java/com/glinboy/jcart/service/impl/SettingServiceImpl.java
@@ -7,13 +7,15 @@ import org.springframework.stereotype.Service;
 import com.glinboy.jcart.model.Setting;
 import com.glinboy.jcart.repository.SettingRepositoryApi;
 import com.glinboy.jcart.service.SettingServiceApi;
+import com.glinboy.jcart.service.dto.SettingDTO;
+import com.glinboy.jcart.service.mapper.SettingMapper;
 
 @Service
 @Transactional
-public class SettingServiceImpl extends AbstractServiceImpl<Setting, SettingRepositoryApi> implements SettingServiceApi {
+public class SettingServiceImpl extends AbstractServiceImpl<SettingDTO, Setting, SettingMapper, SettingRepositoryApi>
+		implements SettingServiceApi {
 
-	public SettingServiceImpl(SettingRepositoryApi repository) {
-		super(repository);
+	public SettingServiceImpl(SettingRepositoryApi repository, SettingMapper mapper) {
+		super(repository, mapper);
 	}
-
 }
