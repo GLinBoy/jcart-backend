410914050873f298eab82cdac7748265810f3229
GLinBoy <hojjatabedie@gmail.com>
2021-07-16 13:38:00 +0430

    👌 IMPROVE: Update return type of register

--- src/main/java/com/glinboy/jcart/service/UserServiceApi.java
+++ src/main/java/com/glinboy/jcart/service/UserServiceApi.java
@@ -1,11 +1,10 @@
 package com.glinboy.jcart.service;
 
-import com.glinboy.jcart.model.User;
 import com.glinboy.jcart.service.dto.UserDTO;
 
 public interface UserServiceApi extends GenericService<UserDTO> {
 
-	User register(UserDTO user);
+	UserDTO register(UserDTO user);
 
 	void recoveryByMail(String mail);
 
