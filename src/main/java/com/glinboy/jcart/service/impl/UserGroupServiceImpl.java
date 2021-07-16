0a9195df7b86e191cd12b1f009666dae7a7e5756
GLinBoy <hojjatabedie@gmail.com>
2021-07-16 13:35:25 +0430

    👌 IMPROVE: Update extends

--- src/main/java/com/glinboy/jcart/service/impl/UserGroupServiceImpl.java
+++ src/main/java/com/glinboy/jcart/service/impl/UserGroupServiceImpl.java
@@ -7,13 +7,16 @@ import org.springframework.stereotype.Service;
 import com.glinboy.jcart.model.UserGroup;
 import com.glinboy.jcart.repository.UserGroupRepositoryApi;
 import com.glinboy.jcart.service.UserGroupServiceApi;
+import com.glinboy.jcart.service.dto.UserGroupDTO;
+import com.glinboy.jcart.service.mapper.UserGroupMapper;
 
 @Service
 @Transactional
-public class UserGroupServiceImpl extends AbstractServiceImpl<UserGroup, UserGroupRepositoryApi>
+public class UserGroupServiceImpl
+		extends AbstractServiceImpl<UserGroupDTO, UserGroup, UserGroupMapper, UserGroupRepositoryApi>
 		implements UserGroupServiceApi {
 
-	public UserGroupServiceImpl(UserGroupRepositoryApi repository) {
-		super(repository);
+	public UserGroupServiceImpl(UserGroupRepositoryApi repository, UserGroupMapper mapper) {
+		super(repository, mapper);
 	}
 }
