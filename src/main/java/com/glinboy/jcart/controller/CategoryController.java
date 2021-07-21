fd76796341bd23203fbc702e8c24aacc33142515
GLinBoy <hojjatabedie@gmail.com>
2021-07-21 17:39:55 +0430

    👌 IMPROVE: getCategories accept/return DTO

--- src/main/java/com/glinboy/jcart/controller/CategoryController.java
+++ src/main/java/com/glinboy/jcart/controller/CategoryController.java
@@ -41,7 +41,7 @@ public class CategoryController {
 	}
 
 	@PostMapping
-	public ProductCategory addCategory(@RequestBody ProductCategory category) {
+	public ProductCategoryDTO addCategory(@RequestBody ProductCategoryDTO category) {
 		return categoryService.save(category);
 	}
 
