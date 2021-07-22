d1c23d44efd617e8bcc246f6fb9374ae16f36be9
GLinBoy <hojjatabedie@gmail.com>
2021-07-22 20:36:49 +0430

    👌 IMPROVE: Change return type to DTO

--- src/main/java/com/glinboy/jcart/service/ContentServiceApi.java
+++ src/main/java/com/glinboy/jcart/service/ContentServiceApi.java
@@ -1,8 +1,7 @@
 package com.glinboy.jcart.service;
 
-import com.glinboy.jcart.model.Content;
 import com.glinboy.jcart.service.dto.ContentDTO;
 
 public interface ContentServiceApi extends GenericService<ContentDTO> {
-	Content getSingleByTitle(String title);
+	ContentDTO getSingleByTitle(String title);
 }
