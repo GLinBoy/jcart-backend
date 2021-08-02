b9e5529a38d19f83d6ebd1d155fc08513d900e12
GLinBoy <hojjatabedie@gmail.com>
2021-08-02 18:43:03 +0430

    👌 IMPROVE: Update getAllDiscount method to return ResponseEntity

--- src/main/java/com/glinboy/jcart/controller/DiscountController.java
+++ src/main/java/com/glinboy/jcart/controller/DiscountController.java
@@ -2,6 +2,7 @@ package com.glinboy.jcart.controller;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,8 +26,8 @@ public class DiscountController {
 	private final DiscountServiceApi discountService;
 
 	@GetMapping
-	public Page<DiscountDTO> getAllDiscount(Pageable pageable) {
-		return discountService.getAll(pageable);
+	public ResponseEntity<Page<DiscountDTO>> getAllDiscount(Pageable pageable) {
+		return ResponseEntity.ok(discountService.getAll(pageable));
 	}
 
 	@GetMapping(path = "/{id}")
