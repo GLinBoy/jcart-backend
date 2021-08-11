68c76497660f05e03c0db79428880ef570aed654
GLinBoy <hojjatabedie@gmail.com>
2021-08-11 14:06:35 +0430

    👌 IMPROVE: Update getUserAddress method to return ResponseEntity

--- src/main/java/com/glinboy/jcart/controller/UserController.java
+++ src/main/java/com/glinboy/jcart/controller/UserController.java
@@ -145,9 +145,9 @@ public class UserController {
 
 	@PreAuthorize("hasAuthority('ROLE_USER')")
 	@GetMapping(path = "/{user_id}/addresses/{address_id}")
-	public UserAddressDTO getUserAddress(@PathVariable("user_id") Long userId,
+	public ResponseEntity<UserAddressDTO> getUserAddress(@PathVariable("user_id") Long userId,
 			@PathVariable("address_id") Long addressId) {
-		return addressService.getAddressByUser(userId, addressId);
+		return ResponseEntity.ok(addressService.getAddressByUser(userId, addressId));
 	}
 
 	// --- cart section
