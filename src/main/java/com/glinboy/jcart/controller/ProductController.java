a09547775895b92d7ccff0ab9aead0ae60068647
GLinBoy <hojjatabedie@gmail.com>
2021-08-13 13:51:48 +0430

    👌 IMPROVE: ProductController extends GenericController

--- src/main/java/com/glinboy/jcart/controller/ProductController.java
+++ src/main/java/com/glinboy/jcart/controller/ProductController.java
@@ -1,55 +1,18 @@
 package com.glinboy.jcart.controller;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.glinboy.jcart.service.ProductServiceApi;
-import com.glinboy.jcart.service.dto.ProductDTO;
 import com.glinboy.jcart.service.dto.ProductShopItemDTO;
-
-import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping(path = "/products")
 @PreAuthorize("hasAuthority('ROLE_ADMIN')")
-@RequiredArgsConstructor
-public class ProductController {
+public class ProductController extends GenericController<ProductShopItemDTO, ProductServiceApi> {
 
-	private final ProductServiceApi productService;
-
-	@GetMapping
-	public ResponseEntity<Page<ProductShopItemDTO>> getProducts(Pageable pageable) {
-		return ResponseEntity.ok(productService.getAll(pageable));
-	}
-
-	@GetMapping(path = "/{id}")
-	public ResponseEntity<ProductDTO> getProduct(@PathVariable("id") Long id) {
-		return ResponseEntity.ok(productService.getSingleById(id));
-	}
-
-	@PostMapping
-	public ResponseEntity<ProductDTO> addProduct(@RequestBody ProductShopItemDTO productDTO) {
-		return ResponseEntity.ok(productService.save(productDTO));
-	}
-
-	@PutMapping
-	public ResponseEntity<ProductDTO> updateProduct(ProductShopItemDTO productDTO) {
-		return ResponseEntity.ok(productService.update(productDTO));
-	}
-
-	@DeleteMapping(path = "/{id}")
-	public ResponseEntity<Void> deleteProduct(@PathVariable("id") Long productId) {
-		productService.deleteSingleById(productId);
-		return ResponseEntity.ok().build();
+	public ProductController(ProductServiceApi service) {
+		super(service);
 	}
 }
