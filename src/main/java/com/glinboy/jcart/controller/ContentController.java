dce2d2f1fde1879072edc86782e361d22d717ad1
GLinBoy <hojjatabedie@gmail.com>
2021-08-15 20:55:46 +0430

    👌 IMPROVE: ContentController extends GenericController

--- src/main/java/com/glinboy/jcart/controller/ContentController.java
+++ src/main/java/com/glinboy/jcart/controller/ContentController.java
@@ -14,39 +14,38 @@ import org.springframework.web.bind.annotation.RestController;
 import com.glinboy.jcart.service.ContentServiceApi;
 import com.glinboy.jcart.service.dto.ContentDTO;
 
-import lombok.RequiredArgsConstructor;
-
 @RestController
 @RequestMapping(path = "/contents")
 @PreAuthorize("hasAuthority('ROLE_ADMIN')")
-@RequiredArgsConstructor
-public class ContentController {
+public class ContentController extends GenericController<ContentDTO, ContentServiceApi> {
 
-	private final ContentServiceApi contentService;
+	public ContentController(ContentServiceApi service) {
+		super(service);
+	}
 
 	@GetMapping(path = "/{id}")
 	public ResponseEntity<ContentDTO> getContentById(@PathVariable Long id) {
-		return ResponseEntity.ok(contentService.getSingleById(id));
+		return ResponseEntity.ok(this.service.getSingleById(id));
 	}
 
 	@GetMapping(path = "/{title}")
 	public ResponseEntity<ContentDTO> getContentById(@PathVariable String title) {
-		return ResponseEntity.ok(contentService.getSingleByTitle(title));
+		return ResponseEntity.ok(this.service.getSingleByTitle(title));
 	}
 
 	@PostMapping
 	public ResponseEntity<ContentDTO> saveContent(@RequestBody ContentDTO contentDTO) {
-		return ResponseEntity.ok(contentService.save(contentDTO));
+		return ResponseEntity.ok(this.service.save(contentDTO));
 	}
 
 	@PutMapping
 	public ResponseEntity<ContentDTO> updateContent(@RequestBody ContentDTO contentDTO) {
-		return ResponseEntity.ok(contentService.update(contentDTO));
+		return ResponseEntity.ok(this.service.update(contentDTO));
 	}
 
 	@DeleteMapping(path = "/{id}")
 	public ResponseEntity<Void> deleteContent(@PathVariable Long id) {
-		contentService.deleteSingleById(id);
+		this.service.deleteSingleById(id);
 		return ResponseEntity.ok().build();
 	}
 }
