16b03369b356dcce4b7b3078cb96a73fd630498d
GLinBoy <hojjatabedie@gmail.com>
2021-08-15 21:12:21 +0430

    👌 IMPROVE: Removed unnecessary methods from OrderController

--- src/main/java/com/glinboy/jcart/controller/OrderController.java
+++ src/main/java/com/glinboy/jcart/controller/OrderController.java
@@ -1,11 +1,6 @@
 package com.glinboy.jcart.controller;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,13 +16,4 @@ public class OrderController extends GenericController<OrderDTO, OrderServiceApi
 		super(service);
 	}
 
-	@GetMapping
-	public ResponseEntity<Page<OrderDTO>> getAllOrders(Pageable pageable) {
-		return ResponseEntity.ok(this.service.getAll(pageable));
-	}
-
-	@GetMapping(path = "/{id}")
-	public ResponseEntity<OrderDTO> getOrder(@PathVariable Long id) {
-		return ResponseEntity.ok(this.service.getSingleById(id));
-	}
 }
