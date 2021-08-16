a0892a260933d160119edb67d68a842ce818ef2c
GLinBoy <hojjatabedie@gmail.com>
2021-08-16 21:02:31 +0430

    👌 IMPROVE: Removed unnecessary methods from TransactionController

--- src/main/java/com/glinboy/jcart/controller/TransactionController.java
+++ src/main/java/com/glinboy/jcart/controller/TransactionController.java
@@ -1,11 +1,6 @@
 package com.glinboy.jcart.controller;
 
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,16 +14,6 @@ public class TransactionController extends GenericController<UserTransactionDTO,
 
 	public TransactionController(UserTransactionServiceApi service) {
 		super(service);
-	}
-
-	@GetMapping
-	public ResponseEntity<Page<UserTransactionDTO>> getAll(Pageable pageable) {
-		return ResponseEntity.ok(this.service.getAll(pageable));
-	}
-
-	@GetMapping(path = "/{id}")
-	public ResponseEntity<UserTransactionDTO> get(@PathVariable Long id) {
-		return ResponseEntity.ok(this.service.getSingleById(id));
 	}
 
 }
