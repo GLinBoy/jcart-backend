3724c72eda6aeb638f6cf8a9dd89fb367741c36d
GLinBoy <hojjatabedie@gmail.com>
2021-09-09 15:32:41 +0430

    👌 IMPROVE: Enabled configuration properties
    #74

--- src/main/java/com/glinboy/jcart/ShopApplication.java
+++ src/main/java/com/glinboy/jcart/ShopApplication.java
@@ -4,9 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
+import com.glinboy.jcart.util.ApplicationProperties;
 
 @SpringBootApplication
 @EnableAutoConfiguration(exclude = ErrorMvcAutoConfiguration.class)
+
+@EnableConfigurationProperties({ApplicationProperties.class})
 public class ShopApplication {
 
 	public static void main(String[] args) {
