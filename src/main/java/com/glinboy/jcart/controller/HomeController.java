4387ea933e6b9d3f235c10c01634904e37a885ee
GLinBoy <hojjatabedie@gmail.com>
2021-09-12 11:31:18 +0430

    👌 IMPROVE: Made HomeController a controller instance of rest controller
    resolved #85

--- src/main/java/com/glinboy/jcart/controller/HomeController.java
+++ src/main/java/com/glinboy/jcart/controller/HomeController.java
@@ -4,13 +4,15 @@ import java.io.IOException;
 
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
+@RequestMapping("/")
 public class HomeController {
 
-	@RequestMapping(value = "/")
+	@GetMapping
 	public void redirect(HttpServletResponse response) throws IOException {
 		response.sendRedirect("/swagger-ui.html");
 	}
