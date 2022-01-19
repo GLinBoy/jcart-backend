35561d3c2615206849a191dedfd0a0258a63604e
GLinBoy <hojjatabedie@gmail.com>
2022-01-19 19:01:58 +0330

    📦 NEW: Added UserTransactionRepositoryApi unit test

---
+++ src/test/java/com/glinboy/jcart/repository/UserTransactionRepositoryApiUnitTest.java
@@ -0,0 +1,19 @@
+package com.glinboy.jcart.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UserTransactionRepositoryApiUnitTest {
+
+	@Test
+	void testFindAllByUser_Id() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindByIdAndUser_Id() {
+		fail("Not yet implemented");
+	}
+
+}
