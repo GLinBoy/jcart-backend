8418be96a2f3f37acc0b61d2f335281de3576dcc
GLinBoy <hojjatabedie@gmail.com>
2022-01-19 19:04:21 +0330

    📦 NEW: Added ProductCategoryRepositoryApi unit test

---
+++ src/test/java/com/glinboy/jcart/repository/ProductCategoryRepositoryApiUnitTest.java
@@ -0,0 +1,19 @@
+package com.glinboy.jcart.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ProductCategoryRepositoryApiUnitTest {
+
+	@Test
+	void testFindAllByParentIsNull() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindAllByParent_Id() {
+		fail("Not yet implemented");
+	}
+
+}
