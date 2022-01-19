bece49b1c5ef66845dbce2d5132c7de01755f30e
GLinBoy <hojjatabedie@gmail.com>
2022-01-19 19:05:02 +0330

    📦 NEW: added ContentRepositoryApi unit test

---
+++ src/test/java/com/glinboy/jcart/repository/ContentRepositoryApiUnitTest.java
@@ -0,0 +1,14 @@
+package com.glinboy.jcart.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ContentRepositoryApiUnitTest {
+
+	@Test
+	void testFindByTitle() {
+		fail("Not yet implemented");
+	}
+
+}
