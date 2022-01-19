a7dbfe538760e26e1dd44c790558a29b22cb95ae
GLinBoy <hojjatabedie@gmail.com>
2022-01-19 19:03:31 +0330

    📦 NEW: Added UserGroupRepositoryApi unit test

---
+++ src/test/java/com/glinboy/jcart/repository/UserGroupRepositoryApiUnitTest.java
@@ -0,0 +1,14 @@
+package com.glinboy.jcart.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UserGroupRepositoryApiUnitTest {
+
+	@Test
+	void testFindByName() {
+		fail("Not yet implemented");
+	}
+
+}
