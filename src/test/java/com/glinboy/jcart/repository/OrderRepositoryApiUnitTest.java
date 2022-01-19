1ff04979b4a10f6ae0af8f9b459f059260c9c5b1
GLinBoy <hojjatabedie@gmail.com>
2022-01-19 19:04:44 +0330

    📦 NEW: Added OrderRepositoryApi unit test

---
+++ src/test/java/com/glinboy/jcart/repository/OrderRepositoryApiUnitTest.java
@@ -0,0 +1,24 @@
+package com.glinboy.jcart.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class OrderRepositoryApiUnitTest {
+
+	@Test
+	void testFindAllByUserId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindByUserIdAndId() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindByUserIdAndStatus() {
+		fail("Not yet implemented");
+	}
+
+}
