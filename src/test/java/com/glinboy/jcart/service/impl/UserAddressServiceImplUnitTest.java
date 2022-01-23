4b15e0ef1a624dd461bec8a90e7cfa27f82d9d78
GLinBoy <hojjatabedie@gmail.com>
2022-01-23 20:31:12 +0330

    📦 NEW: Added UserAddressServiceImpl unit test

---
+++ src/test/java/com/glinboy/jcart/service/impl/UserAddressServiceImplUnitTest.java
@@ -0,0 +1,29 @@
+package com.glinboy.jcart.service.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UserAddressServiceImplUnitTest {
+
+	@Test
+	void testGetUserAddresses() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetAddressByUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddAddressToUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeleteAddress() {
+		fail("Not yet implemented");
+	}
+
+}
