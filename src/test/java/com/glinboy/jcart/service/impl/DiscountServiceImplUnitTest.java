70fe2dd78e631436639fa23658b6d71e8132641f
GLinBoy <hojjatabedie@gmail.com>
2022-01-23 20:29:32 +0330

    📦 NEW: Added DiscountServiceImpl unit test

---
+++ src/test/java/com/glinboy/jcart/service/impl/DiscountServiceImplUnitTest.java
@@ -0,0 +1,14 @@
+package com.glinboy.jcart.service.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DiscountServiceImplUnitTest {
+
+	@Test
+	void testVerify() {
+		fail("Not yet implemented");
+	}
+
+}
