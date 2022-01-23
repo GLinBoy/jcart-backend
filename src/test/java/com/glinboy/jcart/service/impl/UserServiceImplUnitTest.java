dfbe34079d661e13fc87ef603d7b4c5e3790957d
GLinBoy <hojjatabedie@gmail.com>
2022-01-23 20:32:15 +0330

    📦 NEW: Added UserServiceImpl unit test

---
+++ src/test/java/com/glinboy/jcart/service/impl/UserServiceImplUnitTest.java
@@ -0,0 +1,29 @@
+package com.glinboy.jcart.service.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UserServiceImplUnitTest {
+
+	@Test
+	void testDeleteSingleById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRegister() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRecoveryByMail() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRecoveryByMobile() {
+		fail("Not yet implemented");
+	}
+
+}
