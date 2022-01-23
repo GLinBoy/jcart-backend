6007dd96569d1bd39ac2a4dd853a7d23637d7eff
GLinBoy <hojjatabedie@gmail.com>
2022-01-23 20:28:37 +0330

    📦 NEW: Added ContentServiceImpl unit test

---
+++ src/test/java/com/glinboy/jcart/service/impl/ContentServiceImplUnitTest.java
@@ -0,0 +1,14 @@
+package com.glinboy.jcart.service.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ContentServiceImplUnitTest {
+
+	@Test
+	void testGetSingleByTitle() {
+		fail("Not yet implemented");
+	}
+
+}
