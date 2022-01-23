a89d26f925da88c71d0565dde1ac7bc033070a4b
GLinBoy <hojjatabedie@gmail.com>
2022-01-23 20:28:59 +0330

    📦 NEW: Added UserTransactionServiceImpl unit test

---
+++ src/test/java/com/glinboy/jcart/service/impl/CouponServiceImplUnitTest.java
@@ -0,0 +1,19 @@
+package com.glinboy.jcart.service.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CouponServiceImplUnitTest {
+
+	@Test
+	void testVerify() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDisableCoupon() {
+		fail("Not yet implemented");
+	}
+
+}
