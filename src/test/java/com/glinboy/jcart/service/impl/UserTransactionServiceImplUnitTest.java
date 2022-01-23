baaf471cc5e703196a9921f9b8be9f6f3192d3de
GLinBoy <hojjatabedie@gmail.com>
2022-01-23 20:31:56 +0330

    📦 NEW: Added UserTransactionServiceImpl unit test

---
+++ src/test/java/com/glinboy/jcart/service/impl/UserTransactionServiceImplUnitTest.java
@@ -0,0 +1,19 @@
+package com.glinboy.jcart.service.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UserTransactionServiceImplUnitTest {
+
+	@Test
+	void testFindUserTransactions() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindByUserAndTransaction() {
+		fail("Not yet implemented");
+	}
+
+}
