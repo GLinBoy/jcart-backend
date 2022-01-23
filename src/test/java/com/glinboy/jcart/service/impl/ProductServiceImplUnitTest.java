9a1c15dd93845f3978360221915516cb84f845c1
GLinBoy <hojjatabedie@gmail.com>
2022-01-23 20:30:48 +0330

    📦 NEW: Added ProductServiceImpl unit test

---
+++ src/test/java/com/glinboy/jcart/service/impl/ProductServiceImplUnitTest.java
@@ -0,0 +1,14 @@
+package com.glinboy.jcart.service.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ProductServiceImplUnitTest {
+
+	@Test
+	void testGetProductsCategory() {
+		fail("Not yet implemented");
+	}
+
+}
