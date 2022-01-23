1ecf2a73cd900c5fd503d9c53a5ba2d092cca84b
GLinBoy <hojjatabedie@gmail.com>
2022-01-23 20:30:05 +0330

    📦 NEW: Added OrderServiceImpl unit test

---
+++ src/test/java/com/glinboy/jcart/service/impl/OrderServiceImplUnitTest.java
@@ -0,0 +1,39 @@
+package com.glinboy.jcart.service.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class OrderServiceImplUnitTest {
+
+	@Test
+	void testGetUserOrders() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetOrderByUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetCart() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddToCart() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeleteFromCart() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdateOrderItemNumber() {
+		fail("Not yet implemented");
+	}
+
+}
