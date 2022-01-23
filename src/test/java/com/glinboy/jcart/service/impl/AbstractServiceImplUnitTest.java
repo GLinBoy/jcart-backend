e383a557c47cd6a8e3b3bcdd4b36cd143b6f7043
GLinBoy <hojjatabedie@gmail.com>
2022-01-23 20:28:13 +0330

    📦 NEW: Added AbstractServiceImpl unit test

---
+++ src/test/java/com/glinboy/jcart/service/impl/AbstractServiceImplUnitTest.java
@@ -0,0 +1,59 @@
+package com.glinboy.jcart.service.impl;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AbstractServiceImplUnitTest {
+
+	@Test
+	void testSave() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetSingleById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeleteSingleById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeleteSingleByReference() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeleteAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAbstractServiceImpl() {
+		fail("Not yet implemented");
+	}
+
+}
