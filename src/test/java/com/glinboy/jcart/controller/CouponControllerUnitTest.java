2cc5318d90d43d56614a6558969d1cf40572ac19
GLinBoy <hojjatabedie@gmail.com>
2022-01-26 20:14:19 +0330

    👌 IMPROVE: Added CouponController unit test

---
+++ src/test/java/com/glinboy/jcart/controller/CouponControllerUnitTest.java
@@ -0,0 +1,19 @@
+package com.glinboy.jcart.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CouponControllerUnitTest {
+
+	@Test
+	void testDisableCoupon() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testVerifyCoupon() {
+		fail("Not yet implemented");
+	}
+
+}
