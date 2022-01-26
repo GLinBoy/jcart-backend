b78ca151e4b661e2021d185706f2dddd43f40fa5
GLinBoy <hojjatabedie@gmail.com>
2022-01-26 20:13:50 +0330

    👌 IMPROVE: Added DiscountController unit test

---
+++ src/test/java/com/glinboy/jcart/controller/DiscountControllerUnitTest.java
@@ -0,0 +1,14 @@
+package com.glinboy.jcart.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class DiscountControllerUnitTest {
+
+	@Test
+	void testVerifyDiscount() {
+		fail("Not yet implemented");
+	}
+
+}
