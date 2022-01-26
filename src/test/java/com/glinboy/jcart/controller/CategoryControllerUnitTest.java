f31871b5c503c7252cfc9adc0a03df6dda820552
GLinBoy <hojjatabedie@gmail.com>
2022-01-26 20:15:33 +0330

    👌 IMPROVE: Added CategoryController unit test

---
+++ src/test/java/com/glinboy/jcart/controller/CategoryControllerUnitTest.java
@@ -0,0 +1,19 @@
+package com.glinboy.jcart.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CategoryControllerUnitTest {
+
+	@Test
+	void testGetProducts() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetChildes() {
+		fail("Not yet implemented");
+	}
+
+}
