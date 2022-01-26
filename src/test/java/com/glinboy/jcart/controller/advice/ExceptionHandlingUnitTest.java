7f1217646751a9e2016e272f9f3ba2df62d6b0b1
GLinBoy <hojjatabedie@gmail.com>
2022-01-26 20:17:01 +0330

    👌 IMPROVE: Added ExceptionHandling unit test

---
+++ src/test/java/com/glinboy/jcart/controller/advice/ExceptionHandlingUnitTest.java
@@ -0,0 +1,14 @@
+package com.glinboy.jcart.controller.advice;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ExceptionHandlingUnitTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
