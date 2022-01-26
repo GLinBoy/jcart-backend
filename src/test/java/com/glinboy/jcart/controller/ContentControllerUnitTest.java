ebc0716acdb70929d51c0bfdac06f8964c007f68
GLinBoy <hojjatabedie@gmail.com>
2022-01-26 20:14:42 +0330

    👌 IMPROVE: Added ContentController unit test

---
+++ src/test/java/com/glinboy/jcart/controller/ContentControllerUnitTest.java
@@ -0,0 +1,14 @@
+package com.glinboy.jcart.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ContentControllerUnitTest {
+
+	@Test
+	void testGetContentById() {
+		fail("Not yet implemented");
+	}
+
+}
