08b79d79d38de0cdc40d7436e9953c3563c12e45
GLinBoy <hojjatabedie@gmail.com>
2022-01-26 20:15:52 +0330

    👌 IMPROVE: Added AuthController unit test

---
+++ src/test/java/com/glinboy/jcart/controller/AuthControllerUnitTest.java
@@ -0,0 +1,24 @@
+package com.glinboy.jcart.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class AuthControllerUnitTest {
+
+	@Test
+	void testAuthenticateUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRegisterUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testLogoutUser() {
+		fail("Not yet implemented");
+	}
+
+}
