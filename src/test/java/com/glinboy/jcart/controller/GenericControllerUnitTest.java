b9b1e306a9a1a54c42c5ac605627cfaa2b981c02
GLinBoy <hojjatabedie@gmail.com>
2022-01-26 20:13:23 +0330

    👌 IMPROVE: Added GenericController unit test

---
+++ src/test/java/com/glinboy/jcart/controller/GenericControllerUnitTest.java
@@ -0,0 +1,34 @@
+package com.glinboy.jcart.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class GenericControllerUnitTest {
+
+	@Test
+	void testGetAll() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetById() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSave() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeleteById() {
+		fail("Not yet implemented");
+	}
+
+}
