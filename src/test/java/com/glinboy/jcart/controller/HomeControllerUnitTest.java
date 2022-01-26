f49d999828d7b4323b5fd605398f170dde620179
GLinBoy <hojjatabedie@gmail.com>
2022-01-26 20:12:54 +0330

    👌 IMPROVE: Added HomeController unit test

---
+++ src/test/java/com/glinboy/jcart/controller/HomeControllerUnitTest.java
@@ -0,0 +1,14 @@
+package com.glinboy.jcart.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HomeControllerUnitTest {
+
+	@Test
+	void testRedirect() {
+		fail("Not yet implemented");
+	}
+
+}
