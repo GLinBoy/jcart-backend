77ff8e66c555474e565dc8b73b08320aaf4e9a8e
GLinBoy <hojjatabedie@gmail.com>
2022-01-26 20:15:09 +0330

    👌 IMPROVE: Added ContactController unit test

---
+++ src/test/java/com/glinboy/jcart/controller/ContactControllerUnitTest.java
@@ -0,0 +1,19 @@
+package com.glinboy.jcart.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ContactControllerUnitTest {
+
+	@Test
+	void testGetContact() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdateContact() {
+		fail("Not yet implemented");
+	}
+
+}
