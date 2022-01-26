1874c095acec5baf628a209b70997c7613aa3ea7
GLinBoy <hojjatabedie@gmail.com>
2022-01-26 20:12:30 +0330

    👌 IMPROVE: Added UserController unit test

---
+++ src/test/java/com/glinboy/jcart/controller/UserControllerUnitTest.java
@@ -0,0 +1,114 @@
+package com.glinboy.jcart.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class UserControllerUnitTest {
+
+	@Test
+	void testGetUsers() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testUpdateUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRegister() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeleteUser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRecoveryByMail() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRecoveryByMobile() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testLogin() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testLogout() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetUserOrders() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetUserOrder() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetUserAddresses() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetUserAddressLongUserAddressDTO() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeleteAddress() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetUserAddressLongLong() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetUserCart() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testAddOrderItem() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDeleteOrderItem() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testOrderItemNumber() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetUserTransactions() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetUserTransaction() {
+		fail("Not yet implemented");
+	}
+
+}
