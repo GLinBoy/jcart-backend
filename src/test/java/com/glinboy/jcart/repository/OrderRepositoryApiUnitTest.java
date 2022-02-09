a646813ab17b381c2fc52479fe5646e2d2fbb352
GLinBoy <hojjatabedie@gmail.com>
2022-02-09 20:05:58 +0330

    📦 NEW: Setup test class

--- src/test/java/com/glinboy/jcart/repository/OrderRepositoryApiUnitTest.java
+++ src/test/java/com/glinboy/jcart/repository/OrderRepositoryApiUnitTest.java
@@ -1,9 +1,18 @@
 package com.glinboy.jcart.repository;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+import com.glinboy.jcart.model.Order;
+
+@DataJpaTest
 class OrderRepositoryApiUnitTest {
 
 	@Test
