e6fc1583e70e33ecc70f2138d99c2167e706cf89
GLinBoy <hojjatabedie@gmail.com>
2022-02-11 20:39:50 +0330

    👌 IMPROVE: Update AuditingConfig to return username (email) instance of user id
    resolved #173

--- src/main/java/com/glinboy/jcart/configuration/AuditingConfig.java
+++ src/main/java/com/glinboy/jcart/configuration/AuditingConfig.java
@@ -17,15 +17,15 @@ import com.glinboy.jcart.model.UserPrincipal;
 public class AuditingConfig {
 
     @Bean
-    public AuditorAware<Long> auditorProvider() {
+    public AuditorAware<String> auditorProvider() {
         return new SpringSecurityAuditAwareImpl();
     }
 }
 
-class SpringSecurityAuditAwareImpl implements AuditorAware<Long> {
+class SpringSecurityAuditAwareImpl implements AuditorAware<String> {
 
     @Override
-    public Optional<Long> getCurrentAuditor() {
+    public Optional<String> getCurrentAuditor() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
         if (authentication == null ||
@@ -36,6 +36,6 @@ class SpringSecurityAuditAwareImpl implements AuditorAware<Long> {
 
         UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
 
-        return Optional.ofNullable(userPrincipal.getId());
+        return Optional.ofNullable(userPrincipal.getUsername());
     }
 }
