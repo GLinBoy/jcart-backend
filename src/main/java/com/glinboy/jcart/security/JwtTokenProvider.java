7a394dcd7f4d5ea813891b087aef08d3cda872ed
GLinBoy <hojjatabedie@gmail.com>
2022-02-13 19:56:57 +0330

    👌 IMPROVE: Updated JwtTokenProvider to extract email string
    resolved #178

--- src/main/java/com/glinboy/jcart/security/JwtTokenProvider.java
+++ src/main/java/com/glinboy/jcart/security/JwtTokenProvider.java
@@ -70,10 +70,10 @@ public class JwtTokenProvider {
 		response.addCookie(cookie);
 	}
 
-	public Long getUserIdFromJWT(String token) {
+	public String getUserIdFromJWT(String token) {
 		Claims claims = Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(token).getBody();
 
-		return Long.parseLong(claims.getSubject());
+		return claims.getSubject();
 	}
 
 	public String getTokenFromCookie(HttpServletRequest request) {
