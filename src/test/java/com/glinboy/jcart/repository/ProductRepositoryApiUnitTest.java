b725f8331ef509ddc8d99b7cc1945c2f50b74bd4
GLinBoy <hojjatabedie@gmail.com>
2022-10-06 18:37:15 +0200

    👌 IMPROVE: Enabled data JPA test

--- src/test/java/com/glinboy/jcart/repository/ProductRepositoryApiUnitTest.java
+++ src/test/java/com/glinboy/jcart/repository/ProductRepositoryApiUnitTest.java
@@ -3,7 +3,9 @@ package com.glinboy.jcart.repository;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
+@DataJpaTest
 class ProductRepositoryApiUnitTest {
 
 	@Test
