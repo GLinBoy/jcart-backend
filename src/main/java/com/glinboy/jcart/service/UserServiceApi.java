936d077119c22eb320297a4d1bbf63df169aa74a
GLinBoy <hojjatabedie@gmail.com>
2023-01-07 20:43:31 +0100

    👌 IMPROVE: Added UserDetailsService interface to the UserServiceAPI

--- src/main/java/com/glinboy/jcart/service/UserServiceApi.java
+++ src/main/java/com/glinboy/jcart/service/UserServiceApi.java
@@ -1,8 +1,10 @@
 package com.glinboy.jcart.service;
 
+import org.springframework.security.core.userdetails.UserDetailsService;
+
 import com.glinboy.jcart.service.dto.UserDTO;
 
-public interface UserServiceApi extends GenericService<UserDTO> {
+public interface UserServiceApi extends GenericService<UserDTO>, UserDetailsService {
 
 	UserDTO register(UserDTO user);
 
