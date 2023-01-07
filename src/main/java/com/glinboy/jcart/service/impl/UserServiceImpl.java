38d77d524ae089e284293c268e508cb0b7e27206
GLinBoy <hojjatabedie@gmail.com>
2023-01-07 20:44:57 +0100

    Implemented UserDetailsService method into UserServiceImpl

--- src/main/java/com/glinboy/jcart/service/impl/UserServiceImpl.java
+++ src/main/java/com/glinboy/jcart/service/impl/UserServiceImpl.java
@@ -1,10 +1,13 @@
 package com.glinboy.jcart.service.impl;
 
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.glinboy.jcart.model.ResourceNotFoundException;
 import com.glinboy.jcart.model.User;
+import com.glinboy.jcart.model.UserPrincipal;
 import com.glinboy.jcart.repository.UserRepositoryApi;
 import com.glinboy.jcart.service.UserServiceApi;
 import com.glinboy.jcart.service.dto.UserDTO;
@@ -43,5 +46,24 @@ public class UserServiceImpl extends AbstractServiceImpl<UserDTO, User, UserMapp
 				.orElseThrow(() -> new ResourceNotFoundException("User not found with id = " + id));
 		user.setIsDeleted(Boolean.TRUE);
 		repository.save(user);
+	}
+
+	@Override
+	@Transactional
+	public UserDetails loadUserByUsername(String maileOrMobile) throws UsernameNotFoundException {
+		// Let people login with either username or email
+		User user = repository.findByEmailOrMobile(maileOrMobile, maileOrMobile).orElseThrow(
+				() -> new UsernameNotFoundException("User not found with Email or Mobile: " + maileOrMobile));
+
+		return UserPrincipal.create(user);
+	}
+
+	// This method is used by JWTAuthenticationFilter
+	@Transactional
+	public UserDetails loadUserById(Long id) {
+		User user = repository.findById(id)
+				.orElseThrow(() -> new UsernameNotFoundException("User not found with id : " + id));
+
+		return UserPrincipal.create(user);
 	}
 }
