c1a58fca388802b9ec174d1aa60949929da4af35
GLinBoy <hojjatabedie@gmail.com>
2023-01-08 16:50:27 +0100

    🐛 FIX: Replaced WebSecurityConfigurerAdapter & enhanced SecurityConfig
    resolved #232

--- src/main/java/com/glinboy/jcart/configuration/SecurityConfig.java
+++ src/main/java/com/glinboy/jcart/configuration/SecurityConfig.java
@@ -3,9 +3,7 @@ package com.glinboy.jcart.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.authentication.configurers.userdetails.DaoAuthenticationConfigurer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -15,93 +13,72 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.csrf.CsrfFilter;
-import org.zalando.problem.spring.web.advice.security.SecurityProblemSupport;
 
 import com.glinboy.jcart.security.CsrfHeaderFilter;
-import com.glinboy.jcart.security.CustomUserDetailsService;
 import com.glinboy.jcart.security.JwtAuthenticationFilter;
+import com.glinboy.jcart.security.JwtTokenProvider;
+import com.glinboy.jcart.service.UserServiceApi;
 
 import lombok.RequiredArgsConstructor;
+
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
+//@Import(SecurityProblemSupport.class)
 @EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true, prePostEnabled = true)
 public class SecurityConfig {
+	
+//	private final SecurityProblemSupport problemSupport;
+	private final UserServiceApi userService;
+	private final JwtTokenProvider jwtTokenProvider;
 	
 	private static final String[] AUTH_WHITELIST = {
 			"/",
 			"/auth/**",
 			"/users/checkEmailAvailability",
 			"/h2-console/**",
-			// -- Swagger UI v2
-			"/v2/api-docs",
-			"/swagger-resources",
-			"/swagger-resources/**",
-			"/configuration/ui",
-			"/configuration/security",
-			"/swagger-ui.html",
-			"/webjars/**",
 			// -- Swagger UI v3 (OpenAPI)
+			"/swagger-ui.html",
 			"/v3/api-docs/**",
 			"/swagger-ui/**"
 			// other public endpoints of your API may be appended to this array
 	};
 
 	@Bean
-	public JwtAuthenticationFilter jwtAuthenticationFilter() {
-		return new JwtAuthenticationFilter();
+	SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		return http
+			.cors().disable()
+			.csrf().disable()
+			.headers().frameOptions().sameOrigin()
+			.and()
+			// FIXME It seems ZalandoProblem library has issue with JakartaEE 
+//			.exceptionHandling()
+//				.authenticationEntryPoint(problemSupport)
+//				.accessDeniedHandler(problemSupport)
+//			.and()
+			.authorizeHttpRequests(
+					request -> request
+						.requestMatchers(AUTH_WHITELIST).permitAll()
+						.requestMatchers(antMatcher("/h2-console/**")).permitAll()
+						.anyRequest().authenticated()
+					)
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+			.addFilterBefore(new JwtAuthenticationFilter(jwtTokenProvider, userService), UsernamePasswordAuthenticationFilter.class)
+			.addFilterAfter(new CsrfHeaderFilter(), CsrfFilter.class)
+			.userDetailsService(userService)
+			.build();
 	}
 
 	@Bean
-	public DaoAuthenticationConfigurer<AuthenticationManagerBuilder, CustomUserDetailsService>
-		configure(AuthenticationManagerBuilder authenticationManagerBuilder,
-				CustomUserDetailsService customUserDetailsService) throws Exception {
-		return authenticationManagerBuilder
-				.userDetailsService(customUserDetailsService)
-				.passwordEncoder(passwordEncoder());
-	}
-
-	@Bean
-	public PasswordEncoder passwordEncoder() {
+	PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-
-	public SecurityFilterChain securityFilterChain(HttpSecurity http,
-			SecurityProblemSupport problemSupport) throws Exception {
-		http.headers().frameOptions().disable();
-		http.cors().and().csrf().disable()
-			.exceptionHandling()
-			.authenticationEntryPoint(problemSupport)
-			.accessDeniedHandler(problemSupport)
-			.and()
-			.sessionManagement()
-			.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-			.and()
-			.authorizeHttpRequests(requests -> requests
-//					.requestMatchers("/users/{user_id}/**")
-//					.access("@dataGuard.checkUserId(authentication,#user_id)")
-					.requestMatchers(AUTH_WHITELIST).permitAll()
-					.anyRequest().authenticated()
-				);
-		// Add our custom JWT security filter
-		http.addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
-		http.addFilterAfter(new CsrfHeaderFilter(), CsrfFilter.class);
-		return http.build();
-	}
 	
 	@Bean
-	public AuthenticationManager authManager(AuthenticationManagerBuilder auth,
-			DaoAuthenticationProvider authenticationProvider) throws Exception {
-		return auth.authenticationProvider(authenticationProvider).build();
-	}
-	
-	@Bean
-	public DaoAuthenticationProvider authenticationProvider(CustomUserDetailsService detailsService,
-			PasswordEncoder encoder) {
-		DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
-		provider.setPasswordEncoder(encoder);
-		provider.setUserDetailsService(detailsService);
-		return provider;
+	AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+		return authenticationConfiguration.getAuthenticationManager();
 	}
 }
