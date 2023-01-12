61aa37d16c3d7571519e96b3645867f42e9b3b76
GLinBoy <hojjatabedie@gmail.com>
2023-01-12 20:15:16 +0100

    👌 IMPROVE: Disabled Problem advice handler

--- src/main/java/com/glinboy/jcart/controller/advice/ExceptionHandling.java
+++ src/main/java/com/glinboy/jcart/controller/advice/ExceptionHandling.java
@@ -3,7 +3,7 @@ package com.glinboy.jcart.controller.advice;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.zalando.problem.spring.web.advice.ProblemHandling;
 
-@ControllerAdvice
+//@ControllerAdvice
 final class ExceptionHandling implements ProblemHandling {
 
 }
