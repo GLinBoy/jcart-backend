74fe6702b84d30bcf301d38cdc7d2ea5ed0f86ec
GLinBoy <hojjatabedie@gmail.com>
2023-01-20 10:00:51 +0100

    👌 IMPROVE: added token to the swagger ui of MenuController

--- src/main/java/com/glinboy/jcart/controller/MenuController.java
+++ src/main/java/com/glinboy/jcart/controller/MenuController.java
@@ -7,9 +7,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.glinboy.jcart.service.MenuServiceApi;
 import com.glinboy.jcart.service.dto.MenuDTO;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+
 @RestController
 @RequestMapping(path = "/menus")
 @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+@SecurityRequirement(name = "Bearer Authentication")
 public class MenuController extends GenericController<MenuDTO, MenuServiceApi> {
 
 	public MenuController(MenuServiceApi service) {
