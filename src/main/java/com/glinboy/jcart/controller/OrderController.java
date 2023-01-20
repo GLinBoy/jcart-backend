c29b78dc5a761d76c1ad329159eb58993fce3d53
GLinBoy <hojjatabedie@gmail.com>
2023-01-20 10:01:04 +0100

    👌 IMPROVE: added token to the swagger ui of OrderController

--- src/main/java/com/glinboy/jcart/controller/OrderController.java
+++ src/main/java/com/glinboy/jcart/controller/OrderController.java
@@ -7,9 +7,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.glinboy.jcart.service.OrderServiceApi;
 import com.glinboy.jcart.service.dto.OrderDTO;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+
 @RestController
 @RequestMapping(path = "/orders")
 @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+@SecurityRequirement(name = "Bearer Authentication")
 public class OrderController extends GenericController<OrderDTO, OrderServiceApi> {
 
 	public OrderController(OrderServiceApi service) {
