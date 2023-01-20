d028f07f06704805bab3f44668f4730f03a6ce16
GLinBoy <hojjatabedie@gmail.com>
2023-01-20 10:01:37 +0100

    👌 IMPROVE: added token to the swagger ui of TransactionController

--- src/main/java/com/glinboy/jcart/controller/TransactionController.java
+++ src/main/java/com/glinboy/jcart/controller/TransactionController.java
@@ -7,9 +7,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.glinboy.jcart.service.UserTransactionServiceApi;
 import com.glinboy.jcart.service.dto.UserTransactionDTO;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+
 @RestController
 @RequestMapping(path = "/transactions")
 @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+@SecurityRequirement(name = "Bearer Authentication")
 public class TransactionController extends GenericController<UserTransactionDTO, UserTransactionServiceApi> {
 
 	public TransactionController(UserTransactionServiceApi service) {
