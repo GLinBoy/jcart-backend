da66a14a32d5d7a29ffaff48a0fa0c2dcd82d320
GLinBoy <hojjatabedie@gmail.com>
2023-04-08 21:27:07 +0200

    👌 IMPROVE: Replaced javax.* with jakarta.* in SignUpRequest

--- src/main/java/com/glinboy/jcart/security/SignUpRequest.java
+++ src/main/java/com/glinboy/jcart/security/SignUpRequest.java
@@ -1,9 +1,8 @@
 package com.glinboy.jcart.security;
 
-import javax.validation.constraints.Email;
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Size;
-
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
